b137c1bc7d82e58da5f4d1c9336e430ddebf4daf
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-10-16 09:59:25 +0900

    SideMenuItem 아이콘 수정

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -33,20 +33,20 @@ const SideMenu = () => {
         <SideMenuItem imageIcon={"fas fa-folder"} title={"구독"} />
       </SideMenuItems>
       <SideMenuItems>
-        <SideMenuItem imageIcon={"fas fa-house-user"} title={"보관함"} />
-        <SideMenuItem imageIcon={"fas fa-compass"} title={"시청 기록"} />
+        <SideMenuItem imageIcon={"fas fa-database"} title={"보관함"} />
+        <SideMenuItem imageIcon={"fas fa-history"} title={"시청 기록"} />
         <SideMenuItem imageIcon={"fas fa-folder"} title={"나중에 볼 동영상"} />
         <SideMenuItem
-          imageIcon={"fas fa-folder"}
+          imageIcon={"far fa-thumbs-up"}
           title={"좋아요 표시한 동영상"}
         />
       </SideMenuItems>
       <SideMenuItems>
         <SideMenuTitle>구독</SideMenuTitle>
-        <SideMenuItem imageIcon={"fas fa-house-user"} title={"음악"} />
-        <SideMenuItem imageIcon={"fas fa-compass"} title={"스포츠"} />
-        <SideMenuItem imageIcon={"fas fa-folder"} title={"게임"} />
-        <SideMenuItem imageIcon={"fas fa-folder"} title={"영화"} />
+        <SideMenuItem imageIcon={"fas fa-play-circle"} title={"음악"} />
+        <SideMenuItem imageIcon={"fas fa-baseball-ball"} title={"스포츠"} />
+        <SideMenuItem imageIcon={"fas fa-gamepad"} title={"게임"} />
+        <SideMenuItem imageIcon={"fas fa-film"} title={"영화"} />
       </SideMenuItems>
       <SideMenuItems>
         <SideMenuTitle>YOUTUBE 더보기</SideMenuTitle>
@@ -54,17 +54,20 @@ const SideMenu = () => {
           imageIcon={"fas fa-house-user"}
           title={"YouTube Premium"}
         />
-        <SideMenuItem imageIcon={"fas fa-compass"} title={"영화"} />
-        <SideMenuItem imageIcon={"fas fa-folder"} title={"게임"} />
-        <SideMenuItem imageIcon={"fas fa-folder"} title={"실시간"} />
-        <SideMenuItem imageIcon={"fas fa-folder"} title={"학습"} />
-        <SideMenuItem imageIcon={"fas fa-folder"} title={"스포츠"} />
+        <SideMenuItem imageIcon={"fas fa-film"} title={"영화"} />
+        <SideMenuItem imageIcon={"fas fa-gamepad"} title={"게임"} />
+        <SideMenuItem imageIcon={"fas fa-record-vinyl"} title={"실시간"} />
+        <SideMenuItem imageIcon={"far fa-lightbulb"} title={"학습"} />
+        <SideMenuItem imageIcon={"fas fa-baseball-ball"} title={"스포츠"} />
       </SideMenuItems>
       <SideMenuItems>
-        <SideMenuItem imageIcon={"fas fa-house-user"} title={"설정"} />
-        <SideMenuItem imageIcon={"fas fa-compass"} title={"신고 기록"} />
-        <SideMenuItem imageIcon={"fas fa-folder"} title={"고객센터"} />
-        <SideMenuItem imageIcon={"fas fa-folder"} title={"의견 보내기"} />
+        <SideMenuItem imageIcon={"fas fa-cog"} title={"설정"} />
+        <SideMenuItem imageIcon={"far fa-flag"} title={"신고 기록"} />
+        <SideMenuItem imageIcon={"far fa-question-circle"} title={"고객센터"} />
+        <SideMenuItem
+          imageIcon={"fas fa-exclamation-circle"}
+          title={"의견 보내기"}
+        />
       </SideMenuItems>
     </SideMenuContainer>
   );
--- src/components/SideMenuItem/SideMenuItem.jsx
+++ src/components/SideMenuItem/SideMenuItem.jsx
@@ -3,12 +3,13 @@ import styled from "styled-components";
 
 const SideMenuItemContainer = styled.li`
   display: flex;
+  justify-content: start;
   align-items: center;
   height: 4vh;
   font-size: 14px;
   color: #606060;
   background-color: ${(props) => (props.clicked ? "#e5e5e5" : "white")};
-  padding-left: 1.5vw;
+  padding-left: 1.2vw;
   text-align: center;
 
   &:hover {
@@ -18,15 +19,20 @@ const SideMenuItemContainer = styled.li`
 `;
 
 const IconImage = styled.i`
+  width: 1.5rem;
   font-size: 18px;
   margin-right: 1.5rem;
+`;
+
+const IconTitle = styled.p`
+  font-size: 12px;
 `;
 
 const SideMenuItem = ({ imageIcon, title, clicked }) => {
   return (
     <SideMenuItemContainer clicked={clicked}>
       <IconImage className={imageIcon}></IconImage>
-      <h1>{title}</h1>
+      <IconTitle>{title}</IconTitle>
     </SideMenuItemContainer>
   );
 };
